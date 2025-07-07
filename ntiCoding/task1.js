906e2655a05318e5e7fa04ed46b328afff3d4430
Mahmoud Elhoseny <mahmoudelhoseny880@gmail.com>
2025-07-07 16:25:00 +0300

    first upload

---
+++ ntiCoding/task1.js
@@ -0,0 +1,10 @@
+/*
+function checkNum(n){
+
+    typeof(n)=="number"? console.log("is number"):console.log("not number");
+
+
+
+}
+checkNum(5);
+*/
---
+++ ntiCoding/task3.js
@@ -0,0 +1,9 @@
+/*
+const word = prompt("Enter a word:");
+let count = 0;
+
+for (let i = 0; i < word.length; i++) {
+  if (word[i] == "e") count++;
+}
+console.log(`the count e  characters ${count}`);
+*/
